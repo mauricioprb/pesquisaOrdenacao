217811b56c4ec9275ff1fef7712b2e32ad4d63d0
Mauricio Pereira <mauriciopereirabraga@gmail.com>
2022-09-15 00:03:22 -0300

    desafio

---
+++ .avaliacoes/desafio1_ordenacao/Form1.cs
@@ -0,0 +1,29 @@
+namespace desafio1_ordenacao {
+    public partial class Form1 : Form {
+        static List<string> nomeAluno = new List<string>();
+        static List<string> nomeCurso = new List<string>();
+
+        public Form1() {
+            InitializeComponent();
+        }
+        private void btSalvar_Click(object sender, EventArgs e) {
+            Agitacao agitacao = new Agitacao();
+
+            nomeAluno.Add(tbAluno.Text);
+            nomeCurso.Add(tbCurso.Text);
+
+            agitacao.agitacao(nomeAluno);
+        }
+
+        private void btLimpar_Click(object sender, EventArgs e) {
+            tbAluno.Text = "";
+            tbCurso.Text = "";
+            tbExibe.Text = "";
+        }
+
+        private void btListar_Click(object sender, EventArgs e) {
+            Agitacao agitacao = new Agitacao();
+            tbExibe.Text = agitacao.exibir(nomeAluno);
+        }
+    }
+}
