46677722499c675ce36c745ee901c6ea61d1ce2b
Mauricio Pereira <mauriciopereirabraga@gmail.com>
2022-09-15 00:06:09 -0300

    desafio

---
+++ .avaliacoes/desafio1_ordenacao/Program.cs
@@ -0,0 +1,14 @@
+namespace desafio1_ordenacao {
+    internal static class Program {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
