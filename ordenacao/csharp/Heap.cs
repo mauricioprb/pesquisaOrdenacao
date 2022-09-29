25697835c522aaf20660ebdd44e5b62ea1d5d5d6
Mauricio Pereira <mauriciopereirabraga@gmail.com>
2022-09-29 10:40:18 -0300

    heap

---
+++ ordenacao/csharp/Heap.cs
@@ -0,0 +1,27 @@
+﻿namespace heapSort
+{
+    public class Heap
+    {
+        bool estaOrdenado(List<int> lista)
+        {
+
+        }
+
+        bool estaHeapMaximo(List<int> lista)
+        {
+            int i;
+
+            for (i = 0; i < lista.Count - 1; i++)
+            {
+                if ((lista[i] >= lista[i * 2]) && (lista[i] >= lista[i * 2 + 1]))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
