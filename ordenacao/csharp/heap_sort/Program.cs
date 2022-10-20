b84d3a75a5437e6ab700e92ce8b514902546fa1f
mauricioprb <mauriciopereirabraga@gmail.com>
2022-10-20 08:36:48 -0300

    heap

---
+++ ordenacao/csharp/heap_sort/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace heapSort
+{
+    class Program
+    {
+        List<int> listaHeap = new List<int>();
+
+        Console.Write("Quantidade números: ");
+        int qtd = Int32.Parse(Console.ReadLine());
+
+        Util.popularLista(listaHeap, qtd);
+
+        Heap.estaHeapMaximo(listaHeap);
+    }
+}
